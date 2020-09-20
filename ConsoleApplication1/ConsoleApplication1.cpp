a8a0ccd996c952863f67c13bc42e17cb1263cd19
Freim12 <47787135+Freim12@users.noreply.github.com>
2020-09-20 12:39:31 +0300

    1

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+
+void reverse()
+{
+    double x;
+    if (cin >> x)
+    {
+        reverse();
+        cout << x << "\n";
+    }
+}
+
+int main()
+{
+    reverse();
+    return 0;
+}
